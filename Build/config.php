153d40ffe5f3578cb735873a834c56b34bec2da0
aanggurmerah78 <aanggurmerah78@gmail.com>
2025-05-08 18:50:26 +0700

    Add files via upload

---
+++ Build/config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'http://daftar.freelawfounders.org/Build/',
+	'parameter' => 'loncat',
+	'template'  => 'temp.html',
+	'keywords'  => 'list.txt',
+	'sitemap'	=> 'sitemap.txt'
+);
+?>
